37edb5bca62574a4c93c7140721f70e69680101b
ArtemHvozdov <artemhvozdov@gmail.com>
2023-11-09 14:18:52 +0200

    create a server for requests

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # eth-balance-service
+
+Before starting, you need to create a ".env" file in the root directory. 
+In this file, create the variable "API_KEY". 
+This is the value of your API key to access the Infura provider.
+
+By default, the application will launch on port 3000. If you want to use a different port to launch, create a PORT variable with a number value in the ".env" file
+
+After starting the server, two routes are available:
+1. "/" - will display the message "Server is working..."
+2. "/balance/address" - will display a message with the address you specified when requesting and the current balance Wei
+
+To run use the script
+
+npm start
--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 require('dotenv').config();
+
+const express = require('express');
 const { Web3 } = require('web3');
 
-const infuraProviderKey = process.env.API_KEY
-const web3 = new Web3(`https://mainnet.infura.io/v3/${infuraProviderKey}`);
+const app = express();
+
+const providerInfuraKey = process.env.API_KEY;
+const providerUrl = `https://mainnet.infura.io/v3/${providerInfuraKey}`;
+const web3 = new Web3(providerUrl)
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    const messageHello = "Server is working..."
+    res.json(messageHello)
+    console.log(providerInfuraKey)
+})
+
+app.get('/balance/:address', async (req, res) => {
+    const { address } = req.params;
+    "0xE94f1fa4F27D9d288FFeA234bB62E1fBC086CA0c"
+
+    try {
+        const balance = await web3.eth.getBalance(address, "latest")
+        const balanceString = balance.toString();
+        res.json({ address, balance: balanceString + " Wei" });
+    } catch (error) {
+        console.error('Ошибка при запросе баланса:', error);
+        res.status(500).json({error: "Error request of balance"});
+    }
+});
+
+const port = process.env.PORT ||  3000;
+app.listen(port, () => {
+    console.log(`Server was runing on the port ${port}`)
+})
+
 
 // list of wallet addres
 //
@@ -11,14 +44,3 @@ const web3 = new Web3(`https://mainnet.infura.io/v3/${infuraProviderKey}`);
 // 0xB9Ff27C08bE49cF82243380716d655741BE0f4d0
 // 0x498E790dac9f8816f3d4d1E85c7f7560DD125f26
 // 0xB9Ff27C08bE49cF82243380716d655741BE0f4d0
-
-const ethereumAddress  = "0xE94f1fa4F27D9d288FFeA234bB62E1fBC086CA0c";
-
-async function getWalletBalance(addres) {
-    web3.eth.getBalance(addres, 'latest')
-        .then(balance => {
-            console.log("Balance:", balance, "Wei")
-        });
-}
-  
-getWalletBalance(ethereumAddress);
